6ebd84434190490783fa78f582a4050b1205e857
Muralivvrsn <64121609+Muralivvrsn@users.noreply.github.com>
2023-04-27 12:14:13 +0530

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { AiOutlineArrowRight } from "react-icons/ai";
